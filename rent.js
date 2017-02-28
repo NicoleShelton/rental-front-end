8e5257917642b0fbe8be35de292368c2ff3afa72
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-02-28 10:12:28 -0600

    set up cart... have to fix bug

--- rent.js
+++ rent.js
@@ -1,31 +1,46 @@
 function responsive() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
 }
 
- var purchaseItem = {
- 	"total": 0,
- 	"lastTransactionAmount": 0,
- 	"numItems": 0,
- 	//adding items
- 	"add": function(itemCost) { this.total += itemCost; this.lastTransactionAmount = itemCost;},
- 	//scanning items
- 	"scan": function(item, quantity) {
- 		switch(item) {
- 			case "Casual Dress": this.add(120.00 * quantity); break;
- 			case "Cocktail Dress": this.add(100.00 * quantity); break;
- 			case "Evening Dress": this.add(175.00 * quantity); break;
- 			case "Pageant Dress": this.add(150.00 * quantity); break;
-            case "Prom Dress": this.add(200.00 * quantity); break;
-            case "Wedding Dress": this.add(300.00 * quantity); break;
- 		}
- 	},
- }
+var purchaseItem = {
+	"total": 0,
+	"lastTransactionAmount": 0,
+	"numItems": 0,
+	//adding items
+	"add": function (itemCost) {
+		this.total += itemCost;
+		this.lastTransactionAmount = itemCost;
+	},
+	//scanning items
+	"scan": function (item, quantity) {
+		switch (item) {
+			case "Casual Dress":
+				this.add(120.00 * quantity);
+				break;
+			case "Cocktail Dress":
+				this.add(100.00 * quantity);
+				break;
+			case "Evening Dress":
+				this.add(175.00 * quantity);
+				break;
+			case "Pageant Dress":
+				this.add(150.00 * quantity);
+				break;
+			case "Prom Dress":
+				this.add(200.00 * quantity);
+				break;
+			case "Wedding Dress":
+				this.add(300.00 * quantity);
+				break;
+		}
+	},
+}
 
 function showTotal() {
-	document.getElementById('total').innerHTML = "Your total is: $"+ purchaseItem.total.toFixed(2);
+	document.getElementById('total').innerHTML = "Your total is: $" + purchaseItem.total.toFixed(2);
 }
