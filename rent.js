c4d482facbdd1336ac8fc578b4871a16d841c87c
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-02-28 14:09:17 -0600

    added jquery

--- rent.js
+++ rent.js
@@ -7,40 +7,25 @@ function responsive() {
 	}
 }
 
-var purchaseItem = {
-	"total": 0,
-	"lastTransactionAmount": 0,
-	"numItems": 0,
-	//adding items
-	"add": function (itemCost) {
-		this.total += itemCost;
-		this.lastTransactionAmount = itemCost;
-	},
-	//scanning items
-	"scan": function (item, quantity) {
-		switch (item) {
-			case "Casual Dress":
-				this.add(120.00 / 10 * 1.07 * quantity);
-				break;
-			case "Cocktail Dress":
-				this.add(100.00 / 10 * 1.07 * quantity);
-				break;
-			case "Evening Dress":
-				this.add(175.00 / 10 * 1.07 * quantity);
-				break;
-			case "Pageant Dress":
-				this.add(150.00 / 10 * 1.07 * quantity);
-				break;
-			case "Prom Dress":
-				this.add(200.00 / 10 * 1.07 * quantity);
-				break;
-			case "Wedding Dress":
-				this.add(300.00 / 10 * 1.07 * quantity);
-				break;
-		}
-	},
-}
+ var purchaseItem = {
+ 	"total": 0,
+ 	"lastTransactionAmount": 0,
+ 	"numItems": 0,
+ 	//adding items
+ 	"add": function(itemCost) { this.total += itemCost; this.lastTransactionAmount = itemCost; /*this.numItems += quantity;*/},
+ 	//scanning items
+ 	"scan": function(item, quantity) {
+ 		switch(item) {
+ 			case "Casual Dress": this.add(120 / 10 * 1.07 * quantity); break;
+ 			case "Cocktail": this.add(100 / 10 * 1.07 * quantity); break;
+ 			case "Evening Dress": this.add(175 / 10 * 1.07 * quantity); break;
+ 			case "Pageant Dress": this.add(150 / 10 * 1.07 * quantity); break;
+			case "Prom Dress": this.add(200 / 10 * 1.07 * quantity); break;
+			case "Wedding Dress": this.add(300 / 10 * 1.07 * quantity); break;
+ 		}
+ 	},
+ }
 
 function showTotal() {
-	document.getElementById('total').innerHTML = "Your total is: $" + purchaseItem.total.toFixed(2);
+	document.getElementById('total').innerHTML = "Your total is: $"+ purchaseItem.total.toFixed(2);
 }
