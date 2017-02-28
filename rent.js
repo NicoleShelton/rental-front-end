be683c7a3c54987745d627f89fbf51fe745f98aa
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-02-28 10:11:10 -0600

    added fonts and colors to list items

--- rent.js
+++ rent.js
@@ -6,3 +6,26 @@ function responsive() {
         x.className = "topnav";
     }
 }
+
+ var purchaseItem = {
+ 	"total": 0,
+ 	"lastTransactionAmount": 0,
+ 	"numItems": 0,
+ 	//adding items
+ 	"add": function(itemCost) { this.total += itemCost; this.lastTransactionAmount = itemCost;},
+ 	//scanning items
+ 	"scan": function(item, quantity) {
+ 		switch(item) {
+ 			case "Casual Dress": this.add(120.00 * quantity); break;
+ 			case "Cocktail Dress": this.add(100.00 * quantity); break;
+ 			case "Evening Dress": this.add(175.00 * quantity); break;
+ 			case "Pageant Dress": this.add(150.00 * quantity); break;
+            case "Prom Dress": this.add(200.00 * quantity); break;
+            case "Wedding Dress": this.add(300.00 * quantity); break;
+ 		}
+ 	},
+ }
+
+function showTotal() {
+	document.getElementById('total').innerHTML = "Your total is: $"+ purchaseItem.total.toFixed(2);
+}
