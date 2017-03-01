af3ff3e79acd31d057d7a8fca4ad220a2542fbce
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-03-01 10:22:50 -0600

    added carousel to index page

--- rent.js
+++ rent.js
@@ -45,3 +45,18 @@ var purchaseItem = {
 function showTotal() {
 	document.getElementById('total').innerHTML = "Your total is: $" + purchaseItem.total.toFixed(2);
 }
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("animate-fading");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 9000);    
+}
